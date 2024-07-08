6b845279b959a1e5cd4c735f45ca6cd9602e05d8
makardvaj <141413540+makardvaj@users.noreply.github.com>
2024-07-08 12:44:37 +0530

    Update trackers.py
    Modified the function so that it removes commas from the price, and treats the decimal to be floating.
    
    e.g. : 12,333.99 becomes 12333.99, and 1,534.0 becomes 1534.0 .

--- trackers.py
+++ trackers.py
@@ -7,5 +7,15 @@ def trackAmazonINPrice(url):
   response = requests.get(url, headers = headers)
   content = response.content
   soup = BeautifulSoup(content, features = 'html5lib')
-  productPrice = soup.find('span', attrs="a-price-whole").text
+  productPrice = str(soup.find('span', attrs="a-price-whole").text)
+  productPrice = list(productPrice)
+  productPrice.remove(',')
+  productPrice = ''.join(productPrice)
   return float(productPrice)
+
+# DRIVER CODE
+# myurl = "https://www.amazon.in/Peter-Greats-African-Experiments-Prose/dp/1681375990/?_encoding=UTF8&pd_rd_w=8N2xE&content-id=amzn1.sym.f8b2fc0c-779f-43a6-b25a-069849dd23a6%3Aamzn1.symc.fc11ad14-99c1-406b-aa77-051d0ba1aade&pf_rd_p=f8b2fc0c-779f-43a6-b25a-069849dd23a6&pf_rd_r=0S2ME6X11DH65T3YK0V1&pd_rd_wg=89pdj&pd_rd_r=f8e0a608-bbf1-4dfd-a090-a0852cd9f00c&ref_=pd_hp_d_atf_ci_mcx_mr_ca_hp_atf_d"
+# url2 = "https://amzn.in/d/0jfCLZ3b"
+
+# print(trackAmazonINPrice(myurl))
+# print(trackAmazonINPrice(url2))
